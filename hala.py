f1f1379392127daebff3f6c15a3d2460ae4c2877
Daniel Copete <dcopjim3009@g.educaand.es>
2025-09-30 14:01:55 +0200

    primeros commits

---
+++ hala.py
@@ -0,0 +1,10 @@
+
+num1 = input("pon un número 1:")
+num2 = input("pon  número 2:")
+
+if num1 < num2  :
+    print("num1 < num2")
+elif num1 > num2 :
+    print("num1 >num2")
+else:
+    print("num1=num2")
---
+++ mayor_o_menor.py
@@ -0,0 +1,9 @@
+a = int(input("primer número:"))
+b =int(input("segundo número:"))
+numA = int(a)
+if a > b:
+    print ( "número mayor:", a)
+elif b > a :
+    print ("número mayor:", b)
+else :
+    print (a,"=",b, "son iguales")
