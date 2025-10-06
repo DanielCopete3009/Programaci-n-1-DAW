6dd5ea70d1706d2ba94d5f090cc2026942a4c43e
DanielCopete3009 <dcopjim3009@g.educaand.es>
2025-10-06 12:56:38 +0200

    Update tiposdetriangulo.py

--- tiposdetriangulo.py
+++ tiposdetriangulo.py
@@ -3,10 +3,12 @@
 ladoa = int(input("introduzca distancia lado a en cm:"))
 ladob = int(input("introduzca distancia lado b en cm:"))
 ladoc = int(input("introduzca distancia lado c en cm:"))
-
-if ladoa == ladob ==ladoc :
+if ladoa + ladob > c and ladoa + ladoc > ladob and ladob + ladoc > ladoa:
+        if ladoa == ladob ==ladoc :
         print("el triangulo es equilatero")
-elif ladoa == ladob or ladob == ladoc or ladoa == ladoc:
+        elif ladoa == ladob or ladob == ladoc or ladoa == ladoc:
         print("el triangulo es isósceles")
-else :
+        else :
         print("el triangulo es escaleno")
+else:
+        print("los lados no pueden formar un triangulo")
