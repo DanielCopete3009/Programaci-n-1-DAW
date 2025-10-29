50b5272ee9c612be798476fed8face8e3a32aca3
DanielCopete3009 <dcopjim3009@g.educaand.es>
2025-10-29 21:16:08 +0100

    Delete ejercicio28.py

--- ejercicio28.py
+++
@@ -1,6 +0,0 @@
-num1 = int(input("Introduce el numero al que se le  va a sumar solo números pares del 0 al 100:"))
-total = num1
-for i in range(0,100,2):
-        total +=i
-
-print("el resultado es:",total)
