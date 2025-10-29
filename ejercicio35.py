1e1122499f94974609c882fa453b90f82f64e460
DanielCopete3009 <dcopjim3009@g.educaand.es>
2025-10-29 21:14:45 +0100

    Delete ejercicio35.py

--- ejercicio35.py
+++
@@ -1,9 +0,0 @@
-nombre = input("introduce un nombre:")
-clave = input("Introduzca una clave:")
-longitud = len(clave)
-while (len(clave) < 8):
-    print("la contraseña debe tener al menos 8 caracteres")
-    clave = input("introduce una clave de al menos 8 caracteres:")
-
-print("contraseña correcta,","bienvenido", nombre,"!")
-    
