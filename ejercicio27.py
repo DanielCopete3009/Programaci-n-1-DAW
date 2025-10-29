1502e710c1133bd970b32c3e0374582369a3bb69
DanielCopete3009 <dcopjim3009@g.educaand.es>
2025-10-29 21:16:17 +0100

    Delete ejercicio27.py

--- ejercicio27.py
+++
@@ -1,5 +0,0 @@
-num1 = int(input("Introduce un número al que se le va ha sumar los números del 1 al 100:"))
-total = num1
-for i in  range(100,0,-1):
-        total +=i
-print("el resultado de la suma de los números del 1 al 100 es :",total)
