5952f50818ea9786e52fa5c92b4c7a701eb8f100
DanielCopete3009 <dcopjim3009@g.educaand.es>
2025-10-30 21:46:48 +0100

    Delete contarnúmeros.py

--- contarnúmeros.py
+++
@@ -1,9 +0,0 @@
-# Pedir al usuario un número entero positivo
-numero = int(input("Introduce un número entero positivo: "))
-
-contador = 1
-
-# Utilizamos el bucle while para contar hasta el número introducido
-while contador <= numero:
-    print(str(contador))  
-    contador += 1
