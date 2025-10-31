669b54d46d5cef37fff24d72a6f70b40d52df084
DanielCopete3009 <dcopjim3009@g.educaand.es>
2025-10-31 10:17:14 +0100

    Update ejercicio36.py

--- unidad1/ejercicio36.py
+++ unidad1/ejercicio36.py
@@ -10,9 +10,11 @@ raiz_cuadrada = math.sqrt(n1)
 
 potencia = math.pow(n1, 2)
 
+# Mostramos los resultados en pantalla convirtiendo los valores numéricos a texto con str()
 
 print("La raíz cuadrada de " + str(n1) + " es: " + str(raiz_cuadrada))
 print(str(n1) + " elevado al cuadrado es: " + str(potencia))
 
            
+
 
