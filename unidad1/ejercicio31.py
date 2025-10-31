0ecbb91ca841a8402120a023dd8164f230ef007b
DanielCopete3009 <dcopjim3009@g.educaand.es>
2025-10-31 09:48:54 +0100

    Add comments to par_o_impar function
    Added comments to explain function and input.

--- unidad1/ejercicio31.py
+++ unidad1/ejercicio31.py
@@ -1,4 +1,5 @@
-#par o impar
+# Definimos una función llamada 'par_o_impar' que recibe un número como argumento
+
 def par_o_impar(numero):
     if numero % 2 == 0:
           print("el número es par")
@@ -6,5 +7,8 @@ def par_o_impar(numero):
        print("el número es impar")
 
 numero = int(input("introduce un numero:"))
+
+# Llamamos a la función pasándole el número introducido
 (par_o_impar(numero))
+
 
