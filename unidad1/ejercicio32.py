e0601f9c8466581a185f50bf8e045faee1c2a3f6
DanielCopete3009 <dcopjim3009@g.educaand.es>
2025-10-31 09:49:47 +0100

    Enhance comments in ejercicio32.py
    Added comments for clarity on the program's functionality.

--- unidad1/ejercicio32.py
+++ unidad1/ejercicio32.py
@@ -3,7 +3,11 @@
 num1 = float(input("introduce un numero:"))
 numRedondeado = round(num1)
 print(numRedondeado)
+
+# Obtenemos el valor absoluto del número redondeado (si es negativo, lo convierte en positivo)
+
 num1Abs = abs(numRedondeado)
 print(num1Abs)
+
 
 
