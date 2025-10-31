961cbd44d093a902c22fc1f467d1963bb1008231
DanielCopete3009 <dcopjim3009@g.educaand.es>
2025-10-31 10:18:36 +0100

    Update Ejercicio38.py

--- unidad1/Ejercicio38.py
+++ unidad1/Ejercicio38.py
@@ -1,7 +1,15 @@
+# Asignamos un número
+
 a = 5
+
+
+# Caso especial: el 2 es primo
+
 if a = 2: print("Es primo")
 else:
     i = 2
+   
+   # Bucle para comprobar divisores desde 2 hasta a-1
     while i < a:
         if (a%i == 0):
             print ("No Primo")
