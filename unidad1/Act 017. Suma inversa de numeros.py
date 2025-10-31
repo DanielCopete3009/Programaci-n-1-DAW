9f0a0265462db41b6869355077c4b4096f201e1b
DanielCopete3009 <dcopjim3009@g.educaand.es>
2025-10-31 09:37:34 +0100

    Refactor code for summing numbers from 100 to 1

--- unidad1/Act 017. Suma inversa de numeros.py
+++ unidad1/Act 017. Suma inversa de numeros.py
@@ -1,5 +1,6 @@
+#inicializamos la variable suma
 suma = 0
-
+#creamos el bucle desde el 100 al 1
 for i in range(100, 0, -1):
     suma += i
     if i > 1:
@@ -9,3 +10,4 @@ for i in range(100, 0, -1):
   
 print("La suma de los nuemeros del 1 al 100 es:", suma )
     
+
