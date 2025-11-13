a08301a4f71397099f3eee776df0678e2c3d6859
DanielCopete3009 <dcopjim3009@g.educaand.es>
2025-11-13 09:12:25 +0100

    ejercicio9

---
+++ Unidad2/Ejercico9_tabla_alumnos.py
@@ -0,0 +1,8 @@
+nombres = ["Paco","Fran","Juan","Pino"]
+
+print("Alumnos ahora: ", nombres)
+
+nuevo = input("Añade otro alumno: ")
+nombres.append(nuevo)
+
+print("Alumnos actualizados:", nombres)
