64da1ede57ee2a04fefc04957a78ef71ec83f3e6
DanielCopete3009 <dcopjim3009@g.educaand.es>
2025-11-13 09:10:24 +0100

    ejercicio8

---
+++ Unidad2/Ejercicio8.py
@@ -0,0 +1,10 @@
+# Ejemplo dividir array (Slice)
+a = [1,2,3,4,5,6,7,8]
+print(a)
+print(a[0:4:2]) # Cogería los dos primenos números impares
+
+# Ejercicio 8
+nombre = ['D','a','n','i','e ',"l","", 'c','o','p','e','t',"e"]
+print(nombre)
+print(nombre[0:6])
+print(nombre[7:len(nombre)])
