17d1e6c1c8c78abddb971f64dfe7785530d773b0
DanielCopete3009 <dcopjim3009@g.educaand.es>
2025-11-18 13:48:41 +0100

    Ejercicio21

---
+++ Unidad2/ejercicio21.py
@@ -0,0 +1,22 @@
+M = [[1,2,3],[4,5,6],[7,8,9]]
+a = M[1][2]
+print(a)
+b = M[1][1]
+print(b)
+for e in (M[1]):
+    print(e)
+    
+for fila in M:
+    for elemento in fila:
+        print(elemento, end=" ") 
+    print()
+    
+    
+    
+
+
+
+          
+    
+    
+
